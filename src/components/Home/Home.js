8034c74fa6c7219cddb82270e53e83cf16d8ec14
Largenty <57532296+Largenty@users.noreply.github.com>
2021-03-22 16:22:41 +0100

    navBar template

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <main className="red-900">
+      
+p.red-900
+<p className="">fezfez</p>
+      Hello World !
+      
+    </main>
+  )
+}
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <header className="navbar">
+      <nav className="mb-3">
+        <ol className="flex justify-between ">
+          <li className=" link">E-Vins</li>
+          <li className=" link">boutique</li>
+          <li className=" ">
+            <button className="uppercase link">
+              profil
+            </button>{" "}
+            /{" "}
+            <button className="uppercase link">
+              Panié
+            </button>
+          </li>
+        </ol>
+      </nav>
+    </header>
+  );
+}
